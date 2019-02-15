aca90186abe1ece72316fec84336455b70db108f
luca <lhermann@student.42.fr>
2019-02-15 11:44:59 +0100

    [lhermann] - link third party libraries

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 
 import eventsReducer from './reducers/events';
 
@@ -6,8 +6,14 @@ const rootReducer = combineReducers({
   events: eventsReducer
 });
 
+let composeEnhancers = compose;
+
+if (__DEV__) {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
 const configStore = () => {
-  return createStore(rootReducer);
+  return createStore(rootReducer, composeEnhancers());
 };
 
 export default configStore;
