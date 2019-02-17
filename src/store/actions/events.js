4c4c6ac72936a7f1743d3a7c568fd479a3439676
luca <lhermann@student.42.fr>
2019-02-17 23:24:54 +0100

    [lhermann] broke shareEvent

--- src/store/actions/events.js
+++ src/store/actions/events.js
@@ -3,8 +3,8 @@ import { ADD_EVENT, DELETE_EVENT } from './actionTypes';
 export const addEvent = (eventName) => {
   return {
     type: ADD_EVENT,
-    eventName: eventName[0].name,
-    eventDesc: eventName[0].desc
+    eventName: eventName[0].Name,
+    eventDescritpion: eventName[0].Descritpion
   };
 };
 
