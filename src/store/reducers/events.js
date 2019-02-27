616a071d710ad931f4ac02db03394edf2dd2a3dd
luca <lhermann@student.42.fr>
2019-02-27 13:26:17 +0100

    [lhermann] better events.js/reducer

--- src/store/reducers/events.js
+++ src/store/reducers/events.js
@@ -1,23 +1,15 @@
-import { ADD_EVENT, DELETE_EVENT } from '../actions/actionTypes';
+import { DELETE_EVENT, SET_EVENTS } from '../actions/actionTypes';
 
 const initialState = {
   events: []
 }
 
-const eventsReducer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_EVENT:
+    case SET_EVENTS:
       return {
         ...state,
-        events: state.events.concat({
-          key: '' + Math.random(),
-          eventName: action.eventName,
-          eventDescription: action.eventDescription,
-          eventLocation: action.eventLocation,
-          eventImage: { 
-            uri: action.eventImage.uri,
-            base64: action.eventImage.data }
-        })
+        events: action.events
       };
     case DELETE_EVENT:
       return {
@@ -31,4 +23,4 @@ const eventsReducer = (state = initialState, action) => {
   }
 };
 
-export default eventsReducer;
+export default reducer;
