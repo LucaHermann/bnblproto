c2e07759abc225d1343756fc1d206c7a17e98a29
luca <lhermann@student.42.fr>
2019-03-05 12:08:31 +0100

    [lhermann] - better indent

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,9 @@
-export { addEvent, deleteEvent, getEvents } from "./events";
+export {
+  addEvent,
+  deleteEvent,
+  getEvents,
+  eventAdded,
+  startAddEvent
+} from "./events";
 export { tryAuth, authGetToken, authAutoSignIn, authLogout } from "./auth";
 export { uiStartLoading, uiStopLoading } from "./ui";
